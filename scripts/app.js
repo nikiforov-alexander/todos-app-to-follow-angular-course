a4308c3fd554e65acd21143c592f590b2a5a674c
Alexander Nikiforov <nikiforo.alexander@gmail.com>
2016-11-23 12:38:56 +0100

    Add two new Controllers as in S2 of course
    to show prototypical inheritance, and also how one class overrides functions of another class
    
    Also, for the first time we've used double-brackets notation to print variable from controller in template

--- scripts/app.js
+++ scripts/app.js
@@ -2,10 +2,26 @@
 // empty array
 angular.module("tasksListApp", [])
 
-.controller('MainController', function ($scope) {
+    .controller('MainController', function ($scope, $attrs) {
 
-   $scope.helloWorld = function () {
-        console.log("Hello controller");
-   };
+        $scope.helloWorld = function () {
+            console.log("Hello " + $attrs["ngController"]);
+        };
 
-});
+    })
+
+    .controller('CoolController', function ($scope, $attrs) {
+        $scope.whoAmI = function () {
+            console.log("Hello" + $attrs["ngController"]);
+            // if we introduce "helloWorld", it will
+            // will override MainController.helloWorld()
+            // function
+        }
+    })
+
+    // because this one is sibling, he is not going
+    // to override helloWorld() and will be completely
+    // independent
+    .controller('SiblingController', function ($scope) {
+        $scope.foobar = 1234;
+    });
